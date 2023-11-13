15ae150bbd4bccfcbba7216f56191bc98e039e07
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-13 16:04:02 +0100

    giving up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Rhythm from './components/Rhythm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     {/* <Header */}
     {/*  <Products/>*/}
     {/*  <Categories/>*/}
-
+    <Rhythm/>
     </>
   )
 }
---
+++ src/components/Rhythm.jsx
@@ -0,0 +1,38 @@
+import './Rhythm.css'
+
+export default function Brands() {
+
+  return (
+    <>
+    <div className="rhythmsection">
+      <h2 >Why try <span>Rhythm</span></h2>
+      <div className="brandcontainer">
+        <div className="reasons">
+            <div>
+                <div className="rectangle">
+                    <img src="" alt="" />
+                </div>
+                <h4>SMOOTH BROWSING</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <div className="rectangle">
+                    <img src="" alt="" />
+                </div>
+                <h4>SMOOTH BROWSING</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div>
+                <div className="rectangle">
+                    <img src="" alt="" />
+                </div>
+                <h4>SMOOTH BROWSING</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+
+    </div>
+    </div>
+    </>
+  )
+}
